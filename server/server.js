bb0457195c2542ec72e8f2dac376a068eca83be9
akshattyagi03 <tyagiakshat123@gmail.com>
2025-10-30 03:44:18 +0530

    Use permissive CORS to fix deployment issues

--- server/server.js
+++ server/server.js
@@ -29,11 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://minor-project-olive.vercel.app",
-      /https:\/\/.*\.vercel\.app$/
-    ],
+    origin: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
